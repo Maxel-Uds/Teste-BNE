d020f6245814e2f69f97b0455ad82b8678fa21a4
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-05-31 14:03:12 -0300

    Exercício 1 adicionado

---
+++ exercicio1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o número de andares do prédio: ");
+            int andares = int.Parse(Console.ReadLine());
+            int alturaPerAndar = 3;
+
+            Console.WriteLine($"A altura do prédio é igual: {alturaPerAndar * andares} metros");
+        }
+    }
+}
