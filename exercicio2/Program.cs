5a616b89088c243053bb69dd4326dbba4e7b4a9d
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-05-31 14:36:42 -0300

    Exercício 2 adicionado

---
+++ exercicio2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite 4 números separados por espaços: ");
+            string nums = Console.ReadLine();
+            
+            double maior = Char.GetNumericValue(nums[0]);
+            double menor = Char.GetNumericValue(nums[0]);
+            double soma = Char.GetNumericValue(nums[0]);
+            for(int i = 1; i < nums.Length ; i++)
+            {
+                if(Char.GetNumericValue(nums[i]) < menor)
+                {
+                    menor = Char.GetNumericValue(nums[i]);;
+                }
+                else if(Char.GetNumericValue(nums[i]) > maior)
+                {
+                    maior = Char.GetNumericValue(nums[i]);
+                }
+
+                soma += Char.GetNumericValue(nums[i]);
+            }
+
+            Console.WriteLine($"Maior número digitado: {maior}");
+            Console.WriteLine($"Menor número digitado: {menor}");
+            Console.WriteLine($"Média dos números digitados: {(soma/nums.Length).ToString("F2")}");
+        }
+    }
+}
