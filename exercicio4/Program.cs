56fb65515a5845edf31322d16bb2ee4a0798d987
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-05-31 15:19:48 -0300

    Exercício 4 adicionado

---
+++ exercicio4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite 5 números: ");
+            string nums = Console.ReadLine();
+
+            var numsOrdenados = nums.OrderByDescending(p => Char.GetNumericValue(p));
+            foreach(var item in numsOrdenados)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
