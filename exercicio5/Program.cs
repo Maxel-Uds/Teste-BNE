e8b4c8cf03b660400e6685a7658a01961237b484
Maxel Udson Alves da Conceição <maxellopes32@gmail.com>
2021-05-31 15:28:15 -0300

    Exercício 5 adicionado

---
+++ exercicio5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escreva seu texto: ");
+            string texto = Console.ReadLine();
+            int numVogais = 0;
+
+            foreach(char letra in texto)
+            {
+                if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u')
+                {
+                    numVogais++;
+                }
+            }
+
+            Console.WriteLine($"Existem {numVogais} vogais nesse texto.");
+        }
+    }
+}
